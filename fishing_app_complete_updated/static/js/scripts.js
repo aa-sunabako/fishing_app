ec81aebdbc1a0c615d19bde1994f5c366cace3d0
aa-sunabako <mira.fermin4@gmail.com>
2024-07-04 15:07:46 +0900

    Add files via upload

--- fishing_app_complete_updated/static/js/scripts.js
+++ fishing_app_complete_updated/static/js/scripts.js
@@ -2,12 +2,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('JavaScript loaded.');
 
-    const searchInput = document.querySelector('input[type="search"]');
-    searchInput.addEventListener('input', function(event) {
-        const query = event.target.value.trim().toLowerCase(); // trim()で前後の空白を削除する
-        document.querySelectorAll('.card').forEach(card => {
-            const title = card.querySelector('.card-title').textContent.trim().toLowerCase(); // trim()で前後の空白を削除する
-            card.style.display = title.includes(query) ? '' : 'none';
-        });
-    });
+    // const searchInput = document.querySelector('input[type="search"]');
+    // searchInput.addEventListener('input', function(event) {
+    //     const query = event.target.value.trim().toLowerCase(); // trim()で前後の空白を削除する
+    //     document.querySelectorAll('.card').forEach(card => {
+    //         const title = card.querySelector('.card-title').textContent.trim().toLowerCase(); // trim()で前後の空白を削除する
+    //         card.style.display = title.includes(query) ? '' : 'none';
+    //     });
+    // });
 });
